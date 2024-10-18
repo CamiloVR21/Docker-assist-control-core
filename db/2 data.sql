99f8dc78dc43a28536fed27464c7e7b4730ce5a3
Camilo Molina Orth <camilo@bennu.cl>
2024-10-18 12:55:56 -0300

    refactor: db

---
+++ db/2 data.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO gender (id, name) VALUES (2, 'Femenino');
+INSERT INTO gender (id, name) VALUES (1, 'Masculino');
+INSERT INTO marital_status (id, name) VALUES (1,'Casado');
+INSERT INTO marital_status (id, name) VALUES (2,'Viudo');
+INSERT INTO marital_status (id, name) VALUES (3,'Divorciado');
+INSERT INTO contract_type (id, name) VALUES (1,'Fijo');
+INSERT INTO contract_type (id, name) VALUES (2,'Indefinido');
+INSERT INTO contract_type (id, name) VALUES (3,'Obra');
+INSERT INTO job_type (id, name) VALUES (1,'Developer');
+INSERT INTO job_type (id, name) VALUES (2,'Devops');
+INSERT INTO job_type (id, name) VALUES (3,'QA');
+INSERT INTO job_type (id, name) VALUES (4,'Proyect Manager');
